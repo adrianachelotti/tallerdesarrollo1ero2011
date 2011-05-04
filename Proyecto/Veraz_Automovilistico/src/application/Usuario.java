ca742feabd8e31a1839c1296111243bcd847f846
dmpstaltari@gmail.com <dmpstaltari@gmail.com@2872d220-3750-e57c-f3a5-f32e73b12c1a>
2011-05-04 03:51:13 +0000

    Proyecto web realizado en struts

---
+++ Proyecto/Veraz_Automovilistico/src/application/Usuario.java
@@ -0,0 +1,20 @@
+package application;
+
+public class Usuario {
+	String nombre;
+	String password;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
