63fe95cf01968d93a40971df338f23054c02169d
dmpstaltari@gmail.com <dmpstaltari@gmail.com@2872d220-3750-e57c-f3a5-f32e73b12c1a>
2011-05-19 03:55:59 +0000

    Se agregan las clases del dominio.

---
+++ Proyecto/Veraz_Automovilistico/src/vehiculos/domain/ActaDeInfraccion.java
@@ -0,0 +1,26 @@
+package vehiculos.domain;
+
+/**
+ * Modela un acta de infraccion
+ * @author grupo 2
+ *
+ */
+public class ActaDeInfraccion {
+
+	String tipo;
+	Integer numero;
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+		
+}
