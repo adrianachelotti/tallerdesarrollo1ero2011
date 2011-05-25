d89be63236180e6a418c155d0833ba6591e52899
adrianachelotti <adrianachelotti@2872d220-3750-e57c-f3a5-f32e73b12c1a>
2011-05-25 22:51:47 +0000

    registracion

---
+++ Proyecto/ScoringVehicularEstadistico/src/main/java/web/Contactenos.java
@@ -0,0 +1,10 @@
+package web;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public class Contactenos extends WebPage {
+
+	public Contactenos(){
+		
+	}
+}
--- Proyecto/ScoringVehicularEstadistico/src/main/java/web/Login2.java
+++ Proyecto/ScoringVehicularEstadistico/src/main/java/web/Login2.java
@@ -1,11 +1,12 @@
 package web;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
 public class Login2 extends WebPage {
 	
 	public Login2(){
-		
+		add(new BookmarkablePageLink("linkContactenos",Contactenos.class));
 	}
 
 }
